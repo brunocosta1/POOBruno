bbba0f12e3a1cd01d2eb6f21bbdba7adc383658d
brunocosta1 <>
2022-11-16 12:25:47 -0300

    sla

---
+++ Colecoes/Exercicio1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Exercicio1 {
+  public static void main(String[] args) {
+
+    Scanner teclado = new Scanner(System.in);
+
+    String frase = teclado.nextLine();
+    String[] palavras = frase.split(" ");
+
+    Map<String, Integer> mapa = new HashMap<String, Integer>();
+
+    for (String palavra : palavras) {
+    }
+  }
+}
