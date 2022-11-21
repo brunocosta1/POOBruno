2329c33289c6cff4838998f2c875cb507abfe634
brunocosta1 <>
2022-11-21 12:51:09 -0300

    Terminando aula

--- Arquivos/Main.java
+++ Arquivos/Main.java
@@ -9,38 +9,57 @@ class Arquivo{
 
     void lerArquivoBin(){
         try{
-            InputStream input = new FileInputStream("input.txt");
-            int data = input.read();
+            FileInputStream fis = new FileInputStream("input.txt");
+            ObjectInputStream input = new ObjectInputStream(fis);
+            Aluno data = (Aluno)input.readObject();
 
-            while(data != -1){
-                System.out.println((char)data);
-                data = input.read();
-            }
+            System.out.println(data);
+            input.close();
         }catch(Exception e){
             e.printStackTrace();
         }
 
     }
 
-    void escreverArquivoBin(){
+    void escreverArquivoBin(Object o){
         try{
-            OutputStream output = new FileOutputStream("input.txt");
-            int count = 0;
-            while(count < this.msg.length()){
-                output.write(this.msg.charAt(count));
-                count++;
-            }
+            FileOutputStream fos = new FileOutputStream("input.txt");
+            ObjectOutputStream output = new ObjectOutputStream(fos);
+            output.writeObject(o);
+            
+            output.close();
         }catch(IOException e){
             e.printStackTrace();
         }
     }
 }
 
+class Aluno implements Serializable{
+    private String nome;
+    private int idade;
+    private String endereco;
+
+    private static final long serialVersionUID = 201980;
+    
+    Aluno(String n, int id){
+        this.nome = n;
+        this.idade = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome + " Idade: " + this.idade;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        Arquivo a = new Arquivo();
-        a.escreverArquivoBin();
-        a.lerArquivoBin();
+        Arquivo arq = new Arquivo();
+        Aluno al1 = new Aluno("Bruno", 23);
+        //Aluno al2 = new Aluno("João", 14);
+
+        //arq.escreverArquivoBin(al1);
+        //arq.escreverArquivoBin(al2);
+        arq.lerArquivoBin();
     }
 }
